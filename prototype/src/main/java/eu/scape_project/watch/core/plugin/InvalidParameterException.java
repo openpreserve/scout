514c404b0fbc7f7b2e471451ebe0cc26a92211ce
Petar Petrov <pepetrov@gmail.com>
2012-03-13 18:08:41 +0100

    adding some plugin stubs

---
+++ prototype/src/main/java/eu/scape_project/watch/core/plugin/InvalidParameterException.java
@@ -0,0 +1,55 @@
+package eu.scape_project.watch.core.plugin;
+
+/**
+ * This exception is thrown when invalid parameters are supplied to the plugin.
+ * 
+ * @author Petar Petrov <me@petarpetrov.org>
+ * 
+ */
+public class InvalidParameterException extends Exception {
+
+  /**
+   * serial uid.
+   */
+  private static final long serialVersionUID = 3368168052428596424L;
+
+  /**
+   * Default constructor.
+   */
+  public InvalidParameterException() {
+    super();
+  }
+
+  /**
+   * Message constructor.
+   * 
+   * @param msg
+   *          the message
+   */
+  public InvalidParameterException(final String msg) {
+    super(msg);
+  }
+
+  /**
+   * {@link Throwable} constructor.
+   * 
+   * @param cause
+   *          the cause of the exception
+   */
+  public InvalidParameterException(final Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Message and {@link Throwable} constructor.
+   * 
+   * @param msg
+   *          the message of the exception.
+   * @param cause
+   *          the cause of the exception.
+   */
+  public InvalidParameterException(final String msg, final Throwable cause) {
+    super(msg, cause);
+  }
+
+}
---
+++ prototype/src/main/java/eu/scape_project/watch/core/plugin/Plugin.java
@@ -0,0 +1,86 @@
+package eu.scape_project.watch.core.plugin;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Thanks to Luis Faria and Rui Castro for the reference implementation.
+ */
+/**
+ * A Plugin interface for dynamic class loading of plugin components within the
+ * watch component.
+ * 
+ * @author Petar Petrov <me@petarpetrov.org>
+ */
+public interface Plugin {
+
+  /**
+   * Initializes the plugin. This method gets called exactly once by the
+   * PluginManager before the plugin gets executed. Before its calling, the
+   * parameter values will be set. This method is responsible for checking them
+   * and initializing itself accordingly.
+   * 
+   * @throws PluginException
+   *           if something goes wrong during the initialization process.
+   */
+  void init() throws PluginException;
+
+  /**
+   * This method is called exactly once before the system shuts down or when the
+   * component decides this plugin is not needed anymore. The plugin is
+   * responsible for cleaning up and freeing all neeeded resources.
+   * 
+   * @throws PluginException
+   *           if something goes wrong.
+   */
+  void shutdown() throws PluginException;
+
+  /**
+   * Retrieves the name of this plugin.
+   * 
+   * @return the name.
+   */
+  String getName();
+
+  /**
+   * Retrieves the version of this plugin.
+   * 
+   * @return the version.
+   */
+  String getVersion();
+
+  /**
+   * Retrieves a short human readable description of this plugin.
+   * 
+   * @return the description.
+   */
+  String getDescription();
+
+  /**
+   * Retrieves a list with {@link ConfigParameter} objects needed/supported by
+   * this plugin. Note the not all config parameters have to be requried (
+   * {@link ConfigParameter#isRequired()} )
+   * 
+   * @return the list with the parameters.
+   */
+  List<ConfigParameter> getParameters();
+
+  /**
+   * Returns a pre-polulated map with the configuration of this plugin. The keys
+   * are the config parameters and the values are the config values. The plugin
+   * can provide default values, but is not expected to.
+   * 
+   * @return the config of the plugin.
+   */
+  Map<String, String> getParameterValues();
+
+  /**
+   * Sets the config parameters.
+   * 
+   * @param values sets the values.
+   * @throws InvalidParameterException
+   */
+  void setParameterValues(Map<String, String> values) throws InvalidParameterException;
+
+  void execute() throws PluginException;
+}

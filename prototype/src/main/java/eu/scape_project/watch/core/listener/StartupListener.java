1645cf39ab61dd941b77cbe6c304412bc71a936f
Kresimir Duretec <kresimir.duretec@gmail.com>
2012-03-14 16:29:52 +0100

    removing some old components and test, adapting Adaptor to work as Quartz jobs

--- prototype/src/main/java/eu/scape_project/watch/core/listener/StartupListener.java
+++ prototype/src/main/java/eu/scape_project/watch/core/listener/StartupListener.java
@@ -1,59 +1,51 @@
 package eu.scape_project.watch.core.listener;
 
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.scape_project.watch.components.CentralMonitor;
+import eu.scape_project.watch.components.AdaptorJob;
+import eu.scape_project.watch.components.listeners.CollectionProfilerListener;
 import eu.scape_project.watch.core.CoreScheduler;
 
 /**
  * 
  * @author kresimir
- *
+ * 
  */
 public class StartupListener implements ServletContextListener {
 
-	private static final Logger LOG = LoggerFactory.getLogger(StartupListener.class);
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		CoreScheduler cs = CoreScheduler.getCoreScheduler();
-		try {
-			cs.shutdown();
-		} catch (SchedulerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+  private static final Logger LOG = LoggerFactory.getLogger(StartupListener.class);
 
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		LOG.info("Hello from Startup");
-		CoreScheduler cs = CoreScheduler.getCoreScheduler();
-		JobDetail job = newJob(CentralMonitor.class).withIdentity("centralMonitorJob","main group").build();
-		Trigger trigger = newTrigger().withIdentity("myTrigger", "group1")
-					.startNow().withSchedule(simpleSchedule()
-					.withIntervalInSeconds(10)
-					.repeatForever())            
-			        .build();	
-		try {
-			cs.start();
-			cs.scheduleJob(job, trigger);
-		} catch (SchedulerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    CoreScheduler cs = CoreScheduler.getCoreScheduler();
+    cs.shutdown();
+  }
+
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    LOG.info("Hello from Startup");
+    CoreScheduler cs = CoreScheduler.getCoreScheduler();
+    JobDetail job = JobBuilder.newJob(AdaptorJob.class).withIdentity("c3po", "CollectionProfileAdaptors")
+      .usingJobData("adaptor", "eu.scape_project.watch.components.adaptors.c3po.C3POAdaptor").build();
+
+    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "CollectionProfileAdaptors").startNow()
+      .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).repeatForever()).build();
+
+    cs.adddGroupJobListener(new CollectionProfilerListener(), "CollectionProfileAdaptors");
+
+    cs.scheduleJob(job, trigger);
+    cs.start();
+  }
 
 }

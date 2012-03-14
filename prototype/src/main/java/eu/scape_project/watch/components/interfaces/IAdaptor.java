512ce3f9d9ec2cc5281f69552882dfe263bf8133
Kresimir Duretec <kresimir.duretec@gmail.com>
2012-03-14 01:53:52 +0100

    removing old core components code , adding Quartz as a scheduler

--- prototype/src/main/java/eu/scape_project/watch/components/interfaces/IAdaptor.java
+++ prototype/src/main/java/eu/scape_project/watch/components/interfaces/IAdaptor.java
@@ -1,8 +1,6 @@
 package eu.scape_project.watch.components.interfaces;
 
-import java.util.List;
-
-import eu.scape_project.watch.components.elements.Task;
+import org.quartz.Job;
 
 
 /**
@@ -11,27 +9,10 @@ import eu.scape_project.watch.components.elements.Task;
  * @author kresimir
  *
  */
-public interface IAdaptor extends Runnable {
+public interface IAdaptor extends Job {
 
 	
-	/**
-	 * Check if the adaptor can perform a given task
-	 * @param t
-	 * @return
-	 */
-	public boolean checkForTask(Task t);
 	
-	/**
-	 * Add task to the Adaptor
-	 * @param t - Task to be added
-	 */
-	public void addTask(Task t);
-	
-	/**
-	 * Add list of tasks to the adaptor
-	 * @param tasks - Tasks to be added
-	 */
-	public void addTask(List<Task> t);
 	
 	
 	

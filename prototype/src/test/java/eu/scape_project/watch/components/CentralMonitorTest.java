cf3384a0a3c48529f684cfc5779fc97e44dacfc0
Kresimir Duretec <kresimir.duretec@gmail.com>
2012-03-19 17:25:09 +0100

    adding tests

--- prototype/src/test/java/eu/scape_project/watch/components/CentralMonitorTest.java
+++ prototype/src/test/java/eu/scape_project/watch/components/CentralMonitorTest.java
@@ -1,5 +1,26 @@
 package eu.scape_project.watch.components;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import eu.scape_project.watch.components.interfaces.IMonitor;
+import eu.scape_project.watch.core.dao.AsyncRequestDAO;
+
 public class CentralMonitorTest {
 
+  @Test 
+  public void testAddMonitor() {
+    CentralMonitor cm = new CentralMonitor();
+    IMonitor mockMonitor = Mockito.mock(IMonitor.class);
+    cm.addMonitor(mockMonitor);
+    Mockito.verify(mockMonitor).registerCentralMonitor(cm);
+  }
+  
+  @Test
+  public void testRegisterToAsyncRequest() {
+    CentralMonitor cm = new CentralMonitor();
+    AsyncRequestDAO mockAR = Mockito.mock(AsyncRequestDAO.class);
+    cm.registerToAsyncRequest(mockAR);
+    Mockito.verify(mockAR).addDOListener(cm);
+  }
 }
--- prototype/src/test/java/eu/scape_project/watch/core/ComponentContainerTest.java
+++ prototype/src/test/java/eu/scape_project/watch/core/ComponentContainerTest.java
@@ -1,22 +1,64 @@
 package eu.scape_project.watch.core;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import eu.scape_project.watch.components.CentralMonitor;
+import eu.scape_project.watch.components.interfaces.IMonitor;
+import eu.scape_project.watch.core.loader.AdaptorLoader;
 
 public class ComponentContainerTest {
 
-	
-	@Test
-	public void testInit() {
-		
-	}
-	
-	@Test
-	public void testDestroy() {
-		
-	}
-	
-	@Test
-	public void testAddMonitor() {
-		
-	}
+  @Test
+  public void testInit() {
+    ComponentContainer cc = new ComponentContainer();
+    CoreScheduler mockCS = Mockito.mock(CoreScheduler.class);
+    AdaptorLoader mockAL = Mockito.mock(AdaptorLoader.class);
+
+    cc.setCoreScheduler(mockCS);
+    cc.setAdaptorLoader(mockAL);
+
+    cc.init();
+
+    Mockito.verify(mockCS).start();
+    Mockito.verify(mockAL).startLoader();
+  }
+
+  @Test
+  public void testDestroy() {
+    ComponentContainer cc = new ComponentContainer();
+    CoreScheduler mockCS = Mockito.mock(CoreScheduler.class);
+    AdaptorLoader mockAL = Mockito.mock(AdaptorLoader.class);
+
+    cc.setCoreScheduler(mockCS);
+    cc.setAdaptorLoader(mockAL);
+
+    cc.destroy();
+
+    Mockito.verify(mockCS).shutdown();
+    Mockito.verify(mockAL).cancelLoader();
+    
+  }
+
+  @Test
+  public void testAddMonitor() {
+    ComponentContainer cc = new ComponentContainer();
+    CentralMonitor mockCM = Mockito.mock(CentralMonitor.class);
+    CoreScheduler mockCS = Mockito.mock(CoreScheduler.class);
+    
+    cc.setCentralMonitor(mockCM);
+    cc.setCoreScheduler(mockCS);
+    
+    IMonitor mockMonitor = Mockito.mock(IMonitor.class);
+    Mockito.when(mockMonitor.getGroup()).thenReturn("test group");
+    
+    cc.addMonitor(mockMonitor);
+    
+    Mockito.verify(mockCM).addMonitor(mockMonitor);
+    Mockito.verify(mockCS).adddGroupJobListener(mockMonitor, "test group");
+    Mockito.verify(mockMonitor).getGroup();
+    
+    
+    
+  }
 }

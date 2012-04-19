52b24cb584806c8f40594e0d736cae608d238414
Petar Petrov <pepetrov@gmail.com>
2012-04-19 14:54:59 +0200

    adding the pronom project stub

---
+++ adaptors/pronom-adaptor/src/main/java/eu/scape_project/watch/adaptor/pronom/App.java
@@ -0,0 +1,13 @@
+package eu.scape_project.watch.adaptor.pronom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

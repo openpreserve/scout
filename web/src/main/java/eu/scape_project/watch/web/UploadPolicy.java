3b5915aaf3547eacb8146143369766a3afd8448c
Petar Petrov <me@petarpetrov.org>
2012-11-21 13:23:53 +0100

    adding upload policy front end [REF #31]
    adding the front end for the policy upload
    with a functional upload of multiple files
    of type xml or rdf.
    The get file and delete file functionality as
    well as the processing of the policies
    is not yet implemented

--- web/src/main/java/eu/scape_project/watch/web/UploadPolicy.java
+++ web/src/main/java/eu/scape_project/watch/web/UploadPolicy.java
@@ -1,9 +1,21 @@
 package eu.scape_project.watch.web;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.google.inject.Inject;
 
@@ -14,7 +26,7 @@ import eu.scape_project.watch.web.annotations.TemplateSource;
 
 @Path("/upload/policy/new")
 @TemplateSource("uploadPolicy")
-@HttpMethod({HttpMethod.Type.GET, HttpMethod.Type.POST})
+@HttpMethod({ HttpMethod.Type.GET, HttpMethod.Type.POST })
 public class UploadPolicy extends TemplateContext {
 
   @Inject
@@ -23,14 +35,67 @@ public class UploadPolicy extends TemplateContext {
   @Inject
   private HttpServletRequest request;
 
+  private String uploadPath = System.getProperty("user.home") + File.separator + ".scout" + File.separator + "policies";
+
+  public UploadPolicy() {
+    File uploadDir = new File(this.uploadPath);
+    if (!uploadDir.exists()) {
+      uploadDir.mkdirs();
+    }
+  }
+
   @Controller(HttpMethod.Type.POST)
-  boolean redirectPostData() throws IOException {
+  boolean redirectPostData() throws IOException, ServletException {
+
+    if (!ServletFileUpload.isMultipartContent(request)) {
+      throw new IllegalArgumentException(
+          "Request is not multipart, please 'multipart/form-data' enctype for your form.");
+    }
+
+    ServletFileUpload uploadHandler = new ServletFileUpload(new DiskFileItemFactory());
+    PrintWriter writer = response.getWriter();
+    response.setContentType("application/json");
+    JSONArray response = new JSONArray();
+    try {
+      List<FileItem> items = uploadHandler.parseRequest(this.request);
+      for (FileItem item : items) {
+        if (!item.isFormField()) {
+          File file = new File(this.uploadPath, item.getName());
+          item.write(file);
+          JSONObject metadata = new JSONObject();
+          metadata.put("name", item.getName());
+          metadata.put("size", item.getSize());
+          metadata.put("url", this.getContextPath() + this.getMustacheletPath() + "/policies?getfile=" + item.getName());
+          // metadata.put("thumbnail_url", "upload?getthumb=" + item.getName());
+          metadata.put("delete_url", this.getContextPath() + this.getMustacheletPath() + "/policies?delfile=" + item.getName());
+          metadata.put("delete_type", "DELETE");
+          response.put(metadata);
+        }
+      }
+    } catch (FileUploadException e) {
+      throw new RuntimeException(e);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      writer.write(response.toString());
+      writer.close();
+    }
 
     return false;
   }
 
   public String getRedirect() {
     return "/web";
+  }
+
+  private String getFileName(final Part part) {
+    final String partHeader = part.getHeader("content-disposition");
+    for (String content : part.getHeader("content-disposition").split(";")) {
+      if (content.trim().startsWith("filename")) {
+        return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+      }
+    }
+    return null;
   }
 
 }
